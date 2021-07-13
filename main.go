3787db4006655d60aaa496b40b653640d06fae4c
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-07-13 09:12:47 +0200

    more

--- main.go
+++ main.go
@@ -56,14 +56,6 @@ When hitting https://raw.githubusercontent.com/andrewrobinson/imdb/207ba5bd2727d
 
 //http://localhost:3000/static/tt0000075.json
 
-func printResults(results []string) {
-	fmt.Printf("full results:%v\n", results)
-}
-
-func printResultsSoFar(results []string) {
-	fmt.Printf("results so far:%v\n", results)
-}
-
 func main() {
 
 	printFlags := false
@@ -85,17 +77,17 @@ func main() {
 		fmt.Printf("Flags passed: %+v\n", flags)
 	}
 
+	//fake results to test channels
 	var results []string
-
 	strings := [5]string{"a", "b", "c", "d", "e"}
-
 	resultsPipe := make(chan string, len(strings))
 
 	//this is meant to signal "how execution ended", so we know if it is full or partial results being displayed
-	//ie values of "done", "timedOut",
+	//ie values of "done", "timedOut", "sigTerm", "sigInt"
 	finishedProcessingPipe := make(chan string)
 
-	//produce to resultsPipe. Not sure how my actual code would interface with this.
+	//produce fake results to resultsPipe.
+	//processFile(real results) just prints still at this stage.
 	go func() {
 		for _, n := range strings {
 			resultsPipe <- n
@@ -105,11 +97,11 @@ func main() {
 
 		elapsed := time.Since(start)
 		if printDuration {
-			fmt.Printf("finished, elapsed time:%v\n", elapsed)
+			fmt.Printf("processFile finished, elapsed time:%v\n", elapsed)
 		}
 
-		//this does arrive - but before all the  result := <-resultsPipe cases have come in
-		//maybe in my actual code I would get somewhere more delayed to send it from
+		//this works as long as processFile takes a while.
+		//Without processFile, this message can arrive before any case result := <-resultsPipe have fired
 		finishedProcessingPipe <- "done"
 	}()
 
@@ -119,35 +111,32 @@ func main() {
 	shutdownSigInt := make(chan os.Signal)
 	signal.Notify(shutdownSigInt, os.Interrupt, syscall.SIGINT)
 
-	//Wish this L wasn't needed
-L:
+Loop:
 
 	for {
 		select {
 		case <-time.Tick(time.Second * 30):
 			fmt.Println("process timed out")
 
-			//this doesn't arrive - why?
+			// Sending messages to channels inside the case statement doesn't seem to work
+			// shutdownSigTerm <- syscall.SIGTERM
 			// finishedProcessingPipe <- "timeOut"
 
-			//b) this doesn't seemt to trigger the case sig. hence no results printed
-			// shutdownSigTerm <- syscall.SIGTERM
-
-			//so I added this instead. It is what happens at sigTerm
+			//so I added this instead of Tyrone's shutdownSigTerm <- syscall.SIGTERM. It is what happens at sigTerm
 			printResultsSoFar(results)
-			break L
+			break Loop
 		case sigTerm := <-shutdownSigTerm:
 			fmt.Printf("sigTerm signal %s received\n", sigTerm)
 			//this doesn't arrive
 			// finishedProcessingPipe <- "sigTerm"
 			printResultsSoFar(results)
-			break L
+			break Loop
 		case sigInt := <-shutdownSigInt:
 			fmt.Printf("sigInt signal %s received\n", sigInt)
 			//this doesn't arrive
 			// finishedProcessingPipe <- "sigInt"
 			printResultsSoFar(results)
-			break L
+			break Loop
 		case message := <-finishedProcessingPipe:
 			fmt.Printf("finishedProcessingPipe, message:%v\n", message)
 			printResults(results)
@@ -160,34 +149,12 @@ L:
 
 }
 
-func main2() {
+func printResults(results []string) {
+	fmt.Printf("full results:%v\n", results)
+}
 
-	printFlags := false
-	printRows := true
-	printMatches := true
-	printDuration := true
-
-	// plotLookuptemplate := "http://www.omdbapi.com/?i=%s&apikey=591edae0"
-
-	//needs go run webserver.go
-	plotLookuptemplate := "http://localhost:3000/%s.json"
-
-	// plotLookuptemplate := "https://raw.githubusercontent.com/andrewrobinson/imdb/207ba5bd2727dfadb65a3faccd6786a099dce5ef/static/tt0000075.json"
-
-	start := time.Now()
-
-	flags := common.BuildProgramFlags()
-	if printFlags {
-		fmt.Printf("Flags passed: %+v\n", flags)
-	}
-
-	processFile(flags, printRows, printMatches, plotLookuptemplate)
-
-	elapsed := time.Since(start)
-	if printDuration {
-		fmt.Printf("finished, elapsed time:%v\n", elapsed)
-	}
-
+func printResultsSoFar(results []string) {
+	fmt.Printf("results so far:%v\n", results)
 }
 
 func processFile(flags model.ProgramFlags, printRows bool, printMatches bool, plotLookuptemplate string) {
